481eb12fc21f83ecbda40a404bbbd842efeba432
Daniel Caspers <daniel@caspersonline.us>
2017-02-01 21:43:08 -0600

    Filled out "About" page
    - Removed dead contact page
    - Added Attribute Routing Support

--- DigitalInspection/App_Start/RouteConfig.cs
+++ DigitalInspection/App_Start/RouteConfig.cs
@@ -7,17 +7,19 @@ using System.Web.Routing;
 
 namespace DigitalInspection
 {
-    public class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+	public class RouteConfig
+	{
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
-        }
-    }
+			routes.MapMvcAttributeRoutes();
+
+			routes.MapRoute(
+				name: "Default",
+				url: "{controller}/{action}/{id}",
+				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+			);
+		}
+	}
 }
--- DigitalInspection/Controllers/HomeController.cs
+++ DigitalInspection/Controllers/HomeController.cs
@@ -32,15 +32,6 @@ namespace DigitalInspection.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
