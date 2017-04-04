2a95e73a45f311f67a3e82aa32a19a87b0a5bf99
Daniel Caspers <daniel@caspersonline.us>
2017-04-03 22:08:17 -0500

    feat(Image Processing, Image Reduction)

--- DigitalInspection/Services/ImageService.cs
+++ DigitalInspection/Services/ImageService.cs
@@ -4,6 +4,8 @@ using System.Web;
 using System.IO;
 using System.Security.AccessControl;
 using System.Security.Principal;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 
 namespace DigitalInspection.Services
 {
@@ -11,7 +13,7 @@ namespace DigitalInspection.Services
 	{
 		private static readonly string UPLOAD_DIR = "~/Uploads/";
 
-		public static Image SaveImage(HttpPostedFileBase picture, string uploadSubdirectory, string fileNamePrefix)
+		public static Models.Image SaveImage(HttpPostedFileBase picture, string uploadSubdirectory, string fileNamePrefix)
 		{
 			// TODO Improve error handling and prevent NPEs
 			if (picture != null && picture.ContentLength > 0)
@@ -19,9 +21,11 @@ namespace DigitalInspection.Services
 				var imageDirectoryPath = CreateStorageFolders(UPLOAD_DIR, uploadSubdirectory);
 				var imageFileName = fileNamePrefix + "_" + picture.FileName;
 				var imagePath = Path.Combine(imageDirectoryPath, imageFileName);
-				picture.SaveAs(imagePath);
+				// TODO: Allow users of function to pass in image quality, or decide whether or not to save at some setting. Maybe custom enum model for image quality?
+				Bitmap processedImage = ReduceImageSize(picture, 240, 320);
+				processedImage.Save(imagePath);
 
-				return new Image
+				return new Models.Image
 				{
 					Title = imageFileName,
 					ImageUrl = imagePath
@@ -31,7 +35,7 @@ namespace DigitalInspection.Services
 			return null;
 		}
 
-		public static void DeleteImage(Image picture, string subdirectory)
+		public static void DeleteImage(Models.Image picture, string subdirectory)
 		{
 			if (picture != null && File.Exists(picture.ImageUrl))
 			{
@@ -62,6 +66,21 @@ namespace DigitalInspection.Services
 			SecurityIdentifier everyone = new SecurityIdentifier(WellKnownSidType.WorldSid, null);
 			sec.AddAccessRule(new FileSystemAccessRule(everyone, FileSystemRights.Modify | FileSystemRights.Synchronize, InheritanceFlags.ContainerInherit | InheritanceFlags.ObjectInherit, PropagationFlags.None, AccessControlType.Allow));
 			Directory.SetAccessControl(path, sec);
+		}
+
+		// http://stackoverflow.com/a/21394605/2831961
+		private static Bitmap ReduceImageSize(HttpPostedFileBase imageFile, int height, int width)
+		{
+			System.Drawing.Image image = System.Drawing.Image.FromStream(imageFile.InputStream, true, true);
+
+			Bitmap newImg = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+			Graphics newGraphic = Graphics.FromImage(newImg);
+
+			newGraphic.InterpolationMode = InterpolationMode.Bicubic;
+			newGraphic.DrawImage(image, 0, 0, width, height);
+			newGraphic.Dispose();
+
+			return newImg;
 		}
 
 
