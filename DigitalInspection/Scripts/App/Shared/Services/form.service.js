17c87223ba53cdbfdf0ec3cedc752aa7b6acddd3
Daniel Caspers <daniel@caspersonline.us>
2017-04-15 17:20:07 -0500

    feat(Delete Measurement)
    - Added external form submission generic code

---
+++ DigitalInspection/Scripts/App/Shared/Services/form.service.js
@@ -0,0 +1,8 @@
+﻿const FormService = function () { };
+
+FormService.triggerExternalSubmit = function(formId, resourceId) {
+	var formElement = $('#' + formId);
+	var routingAction = formElement.attr('action');
+	formElement.attr('action', routingAction + '/' + resourceId);
+	formElement.submit();
+};
