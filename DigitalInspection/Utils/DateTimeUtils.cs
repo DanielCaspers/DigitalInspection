ba4c1a5fef79e26200d3926fcc6f30abf73d12fb
Daniel Caspers <daniel@caspersonline.us>
2017-06-17 18:28:24 -0500

    chore(Update .NET Version to 4.6.1)
    - Updated for DateTimeOffset class functionality for better time handling to/from UNIX

--- DigitalInspection/Utils/DateTimeUtils.cs
+++ DigitalInspection/Utils/DateTimeUtils.cs
@@ -2,20 +2,24 @@
 
 namespace DigitalInspection.Utils
 {
+	/**
+	 * All dates we handle for now are in Local Time, UTC -6
+	 */
 	public class DateTimeUtils
 	{
+		private static readonly int SECONDS_PER_HOUR = 3600;
+		private static readonly int LOCAL_TIME_OFFSET_IN_HOURS = -6;
+
 		public static DateTime FromUnixTime(long unixTimeInSeconds)
 		{
-			var epoch = new DateTime(1970, 1, 1, 0, 0, 0);
-			return epoch.AddSeconds(unixTimeInSeconds).ToLocalTime();
+			DateTimeOffset dt = DateTimeOffset.FromUnixTimeSeconds(unixTimeInSeconds);
+			return dt.LocalDateTime;
 		}
 
 		public static long ToUnixTime(DateTime dateTime)
 		{
-			// Requires ASP.NET 4.6 target framework. May not be worth upgrading...
-			// return DateTimeOffset.ToUnixTimeSeconds();
-			long timeInSeconds = 0;
-			return timeInSeconds;
+			DateTimeOffset dt = dateTime;
+			return dt.ToUnixTimeSeconds() + (LOCAL_TIME_OFFSET_IN_HOURS * SECONDS_PER_HOUR);
 		}
 	}
 }
