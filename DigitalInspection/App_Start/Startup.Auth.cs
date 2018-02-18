3fcef131e7b58e1e60f45643fadef35b6a767474
Daniel Caspers <daniel@caspersonline.us>
2018-02-18 14:18:33 -0600

    fix(#55 - Auth) - Cookie Regeneration
    The server is currently unable to properly revalidate the cookie if it is present on a validation interval check.
    The fix is to simply evict the cookie on the client at the exact same time we require a validation for logging in again.

--- DigitalInspection/App_Start/Startup.Auth.cs
+++ DigitalInspection/App_Start/Startup.Auth.cs
@@ -6,6 +6,7 @@ using Microsoft.Owin.Security.Cookies;
 using Owin;
 using DigitalInspection.Models;
 using DigitalInspection.Services;
+using System.Configuration;
 
 namespace DigitalInspection
 {
@@ -19,6 +20,9 @@ namespace DigitalInspection
 			app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
 			app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
 
+			var authValidationTimeInMinutes = Double.Parse(ConfigurationManager.AppSettings.Get("MurphyAutomotiveAuthValidationIntervalInMinutes"));
+			var authValidationInterval = TimeSpan.FromMinutes(authValidationTimeInMinutes);
+
 			// Enable the application to use a cookie to store information for the signed in user
 			// and to use a cookie to temporarily store information about a user logging in with a third party login provider
 			// Configure the sign in cookie
@@ -26,13 +30,16 @@ namespace DigitalInspection
 			{
 				AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
 				LoginPath = new PathString("/Account/Login"),
+				ExpireTimeSpan = authValidationInterval,
+
+				// Under the current use, we cannot allow a sliding expiration. This must be in lock step with the validateInterval
+				SlidingExpiration = false,
 				Provider = new CookieAuthenticationProvider
 				{
 					// Enables the application to validate the security stamp when the user logs in.
 					// This is a security feature which is used when you change a password or add an external login to your account.  
 					OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
-						// FIXME: Shorten timeout later
-						validateInterval: TimeSpan.FromDays(600),
+						validateInterval: authValidationInterval,
 						regenerateIdentity: (manager, user) => AuthenticationService.RefreshIdentityAsync(manager, user))
 				}
 			});
