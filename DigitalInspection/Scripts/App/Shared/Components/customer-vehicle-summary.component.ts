8f1d94b5f5ed4acb8e17536eb5375120ea01e83d
Daniel Caspers <daniel@caspersonline.us>
2018-04-11 21:05:15 -0500

    feat(#94 - UX) - First draft of Indication of notes present in work order and inspection item notes
    - Fixes misc JS bug with null colors in the customer vehicle summary component

--- DigitalInspection/Scripts/App/Shared/Components/customer-vehicle-summary.component.ts
+++ DigitalInspection/Scripts/App/Shared/Components/customer-vehicle-summary.component.ts
@@ -1,19 +1,23 @@
 ﻿class CustomerVehicleSummaryComponent {
 	public static setContrast(): void {
 
-		var rgb = $('.car-color-badge').css('background-color');
-		var colors = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+		const rgb = $('.car-color-badge').css('background-color');
+		const colors = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
 
-		var r = colors[1];
-		var g = colors[2];
-		var b = colors[3];
+		if (colors === null || colors.length !== 4) {
+			return;
+		}
+
+		const r = colors[1];
+		const g = colors[2];
+		const b = colors[3];
 
 		// http://www.w3.org/TR/AERT#color-contrast
-		var perceivedBrightness = Math.round(((parseInt(r) * 299) +
+		const perceivedBrightness = Math.round(((parseInt(r) * 299) +
 			(parseInt(g) * 587) +
 			(parseInt(b) * 114)) / 1000);
 
-		var foregroundColor = (perceivedBrightness > 125) ? 'black' : 'white';
+		const foregroundColor = (perceivedBrightness > 125) ? 'black' : 'white';
 
 		$('.car-color-badge').css('color', foregroundColor);
 	}
